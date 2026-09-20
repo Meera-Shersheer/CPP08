c7accd3883204f709b57d3ed782c7ad1899109da
Meera Shersheer <meera04qasemshersheer@gmail.com>
2026-09-20 21:30:42 +0300

    ex00 discovering the appropriete approuch

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,26 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: mshershe <mshershe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/02 23:49:12 by mshershe          #+#    #+#             */
-/*   Updated: 2026/08/02 23:54:35 by mshershe         ###   ########.fr       */
+/*   Updated: 2026/09/20 21:30:02 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once 
+#pragma once
 
+#include <iostream>
+#include <algorithm>
+#include <iterator>
 
 template <typename T>
 
-int easyfind (T container, int val)
+int easyfind (T &container, int val)
 {
-	// for (int i = 0; i < container.size() ;i++)
-	// {
-	// 	if (container[i] == val)
-	// 		return (i);
-	// }
-	// return (-1);
+
+	T::iterator str = container.begin();
+	T::iterator end = container.end() - 1;
+
+
+	for (str != end ;str++)
+	{
+		if (*str == val)
+			return (str);
+	}
 }
 
 
+//iterators and find functions
+// complete reading : https://www.geeksforgeeks.org/cpp/iterators-c-stl/
+//ref :  https://www.geeksforgeeks.org/cpp/c-magicians-stl-algorithms/ 
