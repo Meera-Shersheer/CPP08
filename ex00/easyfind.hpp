70a8dd9acbd6201edbb6f90d9e30e931dce7f9e3
Meera Shersheer <meera04qasemshersheer@gmail.com>
2026-09-20 16:54:26 +0300

    skeliton

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/02 23:49:12 by mshershe          #+#    #+#             */
+/*   Updated: 2026/08/02 23:54:35 by mshershe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once 
+
+
+template <typename T>
+
+int easyfind (T container, int val)
+{
+	// for (int i = 0; i < container.size() ;i++)
+	// {
+	// 	if (container[i] == val)
+	// 		return (i);
+	// }
+	// return (-1);
+}
+
+
