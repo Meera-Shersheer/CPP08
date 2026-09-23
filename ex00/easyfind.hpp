aa822ac233f3f26860a51d5f7d83aa5fc8c2e966
Meera Shersheer <meera04qasemshersheer@gmail.com>
2026-09-24 02:47:38 +0300

    ex00 tests writing

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -3,36 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mshershe <mshershe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/02 23:49:12 by mshershe          #+#    #+#             */
-/*   Updated: 2026/09/20 21:30:02 by mshershe         ###   ########.fr       */
+/*   Updated: 2026/09/24 02:46:08 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
 #include <algorithm>
-#include <iterator>
+#include <stdexcept>
+
 
 template <typename T>
 
-int easyfind (T &container, int val)
+typename T::iterator easyfind (T &container, int val)
 {
-
-	T::iterator str = container.begin();
-	T::iterator end = container.end() - 1;
-
-
-	for (str != end ;str++)
-	{
-		if (*str == val)
-			return (str);
-	}
+	typename T::iterator oc;
+	
+	oc = std::find(container.begin(), container.end(), val);
+	if (oc == container.end())
+		throw std::runtime_error("Not Found");
+	return (oc);
 }
 
 
-//iterators and find functions
-// complete reading : https://www.geeksforgeeks.org/cpp/iterators-c-stl/
-//ref :  https://www.geeksforgeeks.org/cpp/c-magicians-stl-algorithms/ 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,71 @@
 /*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/02 23:48:59 by mshershe          #+#    #+#             */
-/*   Updated: 2026/08/02 23:50:17 by mshershe         ###   ########.fr       */
+/*   Updated: 2026/09/24 02:46:47 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+
+
+
+
+int main(void)
+{
+	std::vector<int> num1 = {10,8 ,9,5,2,54,6,9,3,0, 9, 120, 3, 14};
+	std::list<int> num2 = {10,2 ,9,5,8,54,6,9,3,0, 120};
+	std::deque<int> num3 = {3,8 ,9,5,2,54,6,9,3,0, 90, 8};
+	std::list<int> num5;
+
+	//succesfule searching
+	try
+	{
+		easyfind(num1, 9); 	// check it returns the first occurance in case of duplicates
+		easyfind(num2, 2);
+		easyfind(num3, 3);//ocuurance at the first element
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		easyfind(num1, -4); 		//Not Found case
+	
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	try
+	{
+		easyfind(num2, 120); 		//ocuurance at the last element
+	
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	try
+	{
+		easyfind(num5, 0); 			//empty container
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+	
+
+	
+	return (0);
+}
