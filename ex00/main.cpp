1f067894b8f2d6162975476fc5db9b8f92eb1d8c
Meera Shersheer <meera04qasemshersheer@gmail.com>
2026-09-26 16:06:21 +0300

    ex00 finished

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mshershe <mshershe@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/02 23:48:59 by mshershe          #+#    #+#             */
-/*   Updated: 2026/09/24 02:46:47 by mshershe         ###   ########.fr       */
+/*   Updated: 2026/09/24 20:32:29 by mshershe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,52 @@
 
 int main(void)
 {
-	std::vector<int> num1 = {10,8 ,9,5,2,54,6,9,3,0, 9, 120, 3, 14};
-	std::list<int> num2 = {10,2 ,9,5,8,54,6,9,3,0, 120};
-	std::deque<int> num3 = {3,8 ,9,5,2,54,6,9,3,0, 90, 8};
+	std::vector<int> num1;
+
+	num1.push_back(10);	
+	num1.push_back(8);	
+	num1.push_back(9);	
+	num1.push_back(5);	
+	num1.push_back(2);	
+	num1.push_back(54);	
+	num1.push_back(6);	
+	num1.push_back(9);	
+	num1.push_back(3);	
+	num1.push_back(0);	
+	num1.push_back(9);	
+	num1.push_back(120);	
+	num1.push_back(3);	
+	num1.push_back(14);	
+
+	std::list<int> num2;
+
+	num2.push_back(10);	
+	num2.push_back(2);	
+	num2.push_back(9);	
+	num2.push_back(5);	
+	num2.push_back(8);	
+	num2.push_back(54);	
+	num2.push_back(6);	
+	num2.push_back(9);	
+	num2.push_back(3);	
+	num2.push_back(0);	
+	num2.push_back(120);	
+	
+	std::deque<int> num3;
+	
+	num3.push_back(3);	
+	num3.push_back(8);	
+	num3.push_back(9);	
+	num3.push_back(5);	
+	num3.push_back(2);	
+	num3.push_back(54);	
+	num3.push_back(6);	
+	num3.push_back(9);	
+	num3.push_back(3);	
+	num3.push_back(0);	
+	num3.push_back(90);
+	num3.push_back(8);	
+
 	std::list<int> num5;
 
 	//succesfule searching
@@ -31,7 +74,13 @@ int main(void)
 	{
 		easyfind(num1, 9); 	// check it returns the first occurance in case of duplicates
 		easyfind(num2, 2);
-		easyfind(num3, 3);//ocuurance at the first element
+		std::deque<int>::iterator it = easyfind(num3, 3);//ocuurance at the first element
+		if (it == num3.begin())
+			std::cout<< "The function found the first occurance at the first element succesfully" <<std::endl;
+		else if (it != num3.end())
+			std::cout<< "The first occurance is not at the first element " <<std::endl;
+		else
+			std::cout<< "No occurances are Found" <<std::endl;
 	}
 	catch(const std::exception& e)
 	{
@@ -40,8 +89,14 @@ int main(void)
 	
 	try
 	{
-		easyfind(num1, -4); 		//Not Found case
-	
+		
+		std::vector<int>::iterator  it = easyfind(num1, -4); 		//Not Found case
+		if (it == num1.begin())
+			std::cout<< "The function found the first occurance at the first element succesfully" <<std::endl;
+		else if (it != num1.end())
+			std::cout<< "The first occurance is not at the first element " <<std::endl;
+		else
+			std::cout<< "No occurances are Found" <<std::endl;	
 	}
 	catch(const std::exception& e)
 	{
@@ -50,8 +105,15 @@ int main(void)
 	
 	try
 	{
-		easyfind(num2, 120); 		//ocuurance at the last element
-	
+		
+		std::list<int>::iterator  it = easyfind(num2, 120); 		//ocuurance at the last element
+		if (it == num2.end())
+			std::cout<< "No occurances are Found" <<std::endl;	
+		else if (++it == num2.end() )
+			std::cout<< "The function found the first occurance at the last element succesfully" <<std::endl;
+		else
+			std::cout<< "The first occurance is not at the last element " <<std::endl;
+			
 	}
 	catch(const std::exception& e)
 	{
